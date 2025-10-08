7e49bca0dd3345a5b58534a9074d702619c3ec67
scrwww <nulltillasn@gmail.com>
2025-10-08 13:08:53 -0300

    add script, organizando o tarefacontroller

--- RabbitaskWebAPI/Controllers/TagController.cs
+++ RabbitaskWebAPI/Controllers/TagController.cs
@@ -1,8 +1,37 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RabbitaskWebAPI.Data;
+using RabbitaskWebAPI.DTOs.Common;
+using RabbitaskWebAPI.DTOs.Tag;
 
 namespace RabbitaskWebAPI.Controllers
 {
-    public class TagController : ControllerBase
+    public class TagController : BaseController
     {
+
+        private readonly RabbitaskContext _context;
+        private readonly Services.IUserAuthorizationService _authService;
+
+        public TagController(
+            RabbitaskContext context,
+            Services.IUserAuthorizationService authService,
+            ILogger<TarefaController> logger)
+            : base(logger)
+        {
+            _context = context;
+            _authService = authService;
+        }
+
+        #region PesquisarTag por nome
+        /// <summary>
+        /// 
+        /// </summary>
+        //[HttpGet("{Nome:string}")]
+        //public async Task<ActionResult<ApiResponse<TagDto>>> PesquisarTag(string nmTag)
+        //{
+            
+
+        //}
+        #endregion
     }
 }
